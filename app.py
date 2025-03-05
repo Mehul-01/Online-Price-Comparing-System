dcb055855649aa9b0165214bd9ebbf2c6d1f074b
Mehul-01 <demomehul8@gmail.com>
2025-03-05 14:14:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app.secret_key=os.urandom(24)
 
 mydb = mysql.connector.connect(host="3.108.234.188",
                                user="root",
-                               password="Root123!",
+                               password="simplepassword",
                                database="user_login")
 cursor = mydb.cursor()
 
